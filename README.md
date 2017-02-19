a26037471d670c79fab8dd684eb81443ac3b5bdd
archshift <gh@archshift.com>
2017-02-19 15:24:06 -0800

    Fix interrupts.s to properly declare functions
    Prevents THUMB<->ARM interop failures

--- README.md
+++ README.md
@@ -4,8 +4,4 @@
 
 ## Before use
 
-(You should probably use multirust to make your life easier.)
-
-First, you need to install a cross-compiled version of libcore to your machine.
-- Clone https://github.com/phil-opp/nightly-libcore, and copy thumbv5te-none-eabi.json to that folder.
-- Run nightly-libcore's `install-libcore.sh` script: `multirust run nightly sh install-libcore.sh thumbv5te-none-eabi`
+To run `./build.sh`, you need to install `xargo` and be using `rustup` for managing rust installations.
--- src/interrupts.s
+++ src/interrupts.s
@@ -20,6 +20,8 @@
 .extern handle_pre
 .extern handle_dta
 
+.type wrap_handle_irq, %function
+.func wrap_handle_irq
 wrap_handle_irq:
     sub lr, lr, #4
     stmfd sp!, {r0-r3, r12, lr}
@@ -31,10 +33,16 @@ wrap_handle_irq:
     ldmfd sp!, {r14}
     msr cpsr_cxsf, r14
     ldmfd sp!, {r0-r3, r12, pc}^
+.endfunc
 
+.type wrap_handle_fiq, %function
+.func wrap_handle_fiq
 wrap_handle_fiq:
     subs pc, lr, #4
+.endfunc
 
+.type wrap_handle_swi, %function
+.func wrap_handle_swi
 wrap_handle_swi:
     stmfd sp!, {r0-r3, r12, lr}
 
@@ -50,41 +58,60 @@ wrap_handle_swi:
 
     blx handle_swi
     ldmfd sp!, {r0-r3, r12, pc}^
+.endfunc
 
+.type wrap_handle_und, %function
+.func wrap_handle_und
 wrap_handle_und:
     stmfd sp!, {r0-r3, r12, lr}
     sub r0, lr, #4 ;; addr = LR - 4
     blx handle_und
     ldmfd sp!, {r0-r3, r12, pc}^
     movs pc, lr
+.endfunc
 
+.type wrap_handle_pre, %function
+.func wrap_handle_pre
 wrap_handle_pre:
     blx handle_pre
     subs pc, lr, #4
+.endfunc
 
+.type wrap_handle_dta, %function
+.func wrap_handle_dta
 wrap_handle_dta:
     stmfd sp!, {r0-r3, r12, lr}
     sub r0, lr, #8 ;; addr = LR - 4
     blx handle_dta
     ldmfd sp!, {r0-r3, r12, pc}^
     subs pc, lr, #8
+.endfunc
 
+.type enable_interrupts, %function
+.func enable_interrupts
 enable_interrupts:
     mrs r0, cpsr
     bic r0, r0, #((1 << 6) | (1 << 7))
     msr cpsr_c, r0
     bx lr
+.endfunc
 
+.type disable_interrupts, %function
+.func disable_interrupts
 disable_interrupts:
     mrs r0, cpsr
     orr r0, r0, #((1 << 6) | (1 << 7))
     msr cpsr_c, r0
     bx lr
+.endfunc
 
+.type wait_for_interrupt, %function
+.func wait_for_interrupt
 wait_for_interrupt:
     mov r0, #0
     mcr p15, 0, r0, c7, c0, 4
     bx lr
+.endfunc
 
 ldr_pc_pc_neg4:
     ldr pc, [pc, #-4]
