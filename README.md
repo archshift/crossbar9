4b0f0c0f38c90aeb9e75a2c80e1d0c8067847cb8
archshift <gh@archshift.com>
2017-02-19 15:34:34 -0800

    Usability instructions?

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# crossbar9 
+# crossbar9
 
-### Cross-compiled rust template for the 3DS ARM9
+### Cross-compiled tests for the 3DS ARM9
 
 ## Before use
 
 To run `./build.sh`, you need to install `xargo` and be using `rustup` for managing rust installations.
+
+## Usage
+
+```
+./build.sh [test name] [debug|release]
+```
+
+Copy the generated file `./target/thumbv5te-none-eabi/<relmode>/crossbar9.3dsx` to your 3DS's SD card, and run using the Homebrew launcher.
--- build.sh
+++ build.sh
@@ -18,21 +18,21 @@ else
 fi
 
 export CC=arm-none-eabi-gcc
-rustup run nightly xargo build --features "$FEATURE" $RELMODE_FLAG || exit
+rustup run nightly xargo build --features "$FEATURE" $RELMODE_FLAG || exit -1
 
 TARGET_DIR="./target/${TARGET}/${RELMODE}"
-TARGET_ELF="${TARGET_DIR}/3dstest"
-TARGET_BIN="${TARGET_DIR}/3dstest.bin"
-TARGET_3DSX="${TARGET_DIR}/3dstest.3dsx"
+TARGET_ELF="${TARGET_DIR}/crossbar9"
+TARGET_BIN="${TARGET_DIR}/crossbar9.bin"
+TARGET_3DSX="${TARGET_DIR}/crossbar9.3dsx"
 
-arm-none-eabi-objcopy -O binary -I elf32-littlearm "$TARGET_ELF" "$TARGET_BIN" || exit
+arm-none-eabi-objcopy -O binary -I elf32-littlearm "$TARGET_ELF" "$TARGET_BIN" || exit -1
 
 LDR_APPINFO_FILE="./BrahmaLoader/resources/AppInfo"
 LDR_PAYLOAD_FILE="./BrahmaLoader/data/payload.bin"
 LDR_OUTPUT_DIR="./BrahmaLoader/output"
 
-cp "$TARGET_BIN" "$LDR_PAYLOAD_FILE"
-cp ./AppInfo "$LDR_APPINFO_FILE"
-( cd ./BrahmaLoader && make )
-cp -R "${LDR_OUTPUT_DIR}/" "$TARGET_DIR"
-rm -r "${LDR_OUTPUT_DIR}"
+cp "$TARGET_BIN" "$LDR_PAYLOAD_FILE" || exit -1
+cp ./AppInfo "$LDR_APPINFO_FILE" || exit -1
+( cd ./BrahmaLoader && make ) || exit -1
+cp -R "${LDR_OUTPUT_DIR}/" "$TARGET_DIR" || exit -1
+rm -r "${LDR_OUTPUT_DIR}" || exit -1
